d4e05ffb39e37d037a81a19b55fabd0632fb1b8a
linux_china <libing.chen@gmail.com>
2019-10-03 13:43:30 -0700

    add testTemperature

--- src/test/java/org/mvnsearch/measurement/USCSUnitsTest.java
+++ src/test/java/org/mvnsearch/measurement/USCSUnitsTest.java
@@ -7,6 +7,7 @@ import tech.units.indriya.quantity.Quantities;
 import tech.units.indriya.unit.Units;
 
 import javax.measure.quantity.Length;
+import javax.measure.quantity.Temperature;
 
 /**
  * USCS Units
@@ -20,5 +21,12 @@ public class USCSUnitsTest {
         ComparableQuantity<Length> length = Quantities.getQuantity(1.0, USCSUnits.FOOT);
         ComparableQuantity<Length> lengthSI = Quantities.getQuantity(0.3048, Units.METRE);
         Assertions.assertTrue(length.to(Units.METRE).isEquivalentTo(lengthSI));
+    }
+
+    @Test
+    public void testTemperature() {
+        ComparableQuantity<Temperature> temp = Quantities.getQuantity(85, USCSUnits.Fahrenheit);
+        System.out.println(temp.to(Units.KELVIN));
+        System.out.println(Quantities.getQuantity(302.5944444444445, Units.KELVIN).to(USCSUnits.Fahrenheit));
     }
 }
